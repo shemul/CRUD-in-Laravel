8c91a126efce557f5ada3d214b24d70fd1908fc9
Shemulinux <shemulworld@gmail.com>
2014-07-25 19:52:56 +0600

    git Initialization

---
+++ controllers/appController.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+* 
+*/
+class appController extends BaseController
+{
+	
+	public function getIndex()
+	{
+		return "hello world ";
+	}
+
+	public function getUsers()
+	{
+		return "User Pages";
+	}
+}
+?>
